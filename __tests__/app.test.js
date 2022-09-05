fc741353ddbbf2d28255e399aacd3015fd4b1cf3
Adam Tai <adamtai@hotmail.co.uk>
2022-09-05 14:50:27 +0100

    GET categories tests/functionality created

---
+++ __tests__/app.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+const app = require("../app");
+afterAll(() => db.end());
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+describe.only("api/categories", () => {
+  describe("GET", () => {
+    test("should return an array of category objects, each of which should have the following properties: slug, description ", () => {
+      return request(app)
+        .get("/api/categories")
+        .expect(200)
+        .then(({ body }) => {
+          expect(Array.isArray(body.categories)).toBe(true);
+          expect(body.categories.length === 4).toBe(true);
+          body.categories.forEach((category) => {
+            expect(category).toHaveProperty("slug");
+            expect(category).toHaveProperty("description");
+          });
+        });
+    });
+  });
+});

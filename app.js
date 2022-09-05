c3d5ebe97b8d9c94f8456c41f68571a5b6777591
Adam Tai <adamtai@hotmail.co.uk>
2022-09-05 15:49:15 +0100

    get request added to app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { getCategories } = require("./controllers/categories.controller");
+const { getReviews } = require("./controllers/reviews.controller");
 const app = express();
 
 app.get("/api/categories", getCategories);
+
+app.get("/api/reviews/:review_id", getReviews);
 
 app.use((err, req, res, next) => {
   console.log(err, "<<<< Error");

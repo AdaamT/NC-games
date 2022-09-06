22f5800c22b37e72f08979a90b69a995cc4c37f3
Adam Tai <adamtai@hotmail.co.uk>
2022-09-06 16:46:01 +0100

    merging changes

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
+
 const { getCategories } = require("./controllers/categories.controller");
+
+const { getReviewById } = require("./controllers/reviews.controller");
+
+const { getUsers } = require("./controllers/users.controller");
+
+const {
+  handlePsqlErrors,
+  handleServerErrors,
+  handleCustomErrors,
+} = require("./errors/index.js");
+
 const app = express();
 
 app.get("/api/categories", getCategories);
 
-app.use((err, req, res, next) => {
-  console.log(err, "<<<< Error");
-  res.status(500).send({ msg: "Internal Server Error" });
-});
+app.get("/api/reviews/:review_id", getReviewById);
+
+app.get("/api/users", getUsers);
+
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handleServerErrors);
 
 module.exports = app;

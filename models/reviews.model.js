c8c180017d45388fabc243bf1b8250e9985f3286
Adam Tai <adamtai@hotmail.co.uk>
2022-09-06 12:34:50 +0100

    functionality created - promise.reject

--- models/reviews.model.js
+++ models/reviews.model.js
@@ -8,6 +8,9 @@ exports.selectReviewById = (reviewId) => {
     [reviewId]
   );
   return db.query(formatQuery).then((results) => {
+    if (results.rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "Bad request" });
+    }
     return results.rows[0];
   });
 };

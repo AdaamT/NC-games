049bff6909cea0fbe3c90111fad27dac3c4e4acc
Adam Tai <adamtai@hotmail.co.uk>
2022-10-31 23:36:25 +0000

    ticket 9 added

--- models/reviews.model.js
+++ models/reviews.model.js
@@ -73,19 +73,13 @@ exports.updateVoteCount = (reviewId, voteCount) => {
     });
 };
 
-exports.selectReviewById = (id) => {
+exports.selectCommentsById = (review_id) => {
   return db
     .query(
-      "SELECT reviews.*, COUNT (comment_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id",
-      [id]
+      `SELECT * FROM comments WHERE review_id = $1 ORDER BY created_at ASC`,
+      [review_id]
     )
     .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: "Review Id does not exist",
-        });
-      }
-      return rows[0];
+      return rows;
     });
 };
